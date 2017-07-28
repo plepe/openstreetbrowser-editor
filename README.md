8a73a8def13c182c81ef22a2f3dce17f13ad327b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-28 06:56:38 +0200

    Configuration, Installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # openstreetbrowser-editor
 An editor for OpenStreetBrowser categories
+
+# INSTALL
+```sh
+git clone https://github.com/plepe/openstreetbrowser-editor.git
+cd openstreetbrowser-editor
+git submodule init
+git submodule update
+cp conf.php-dist conf.php
+nano conf.php
+```
---
+++ modulekit.php
@@ -0,0 +1,18 @@
+<?php
+$name = "OpenStreetBrowser-editor";
+$id = "openstreetbrowser-editor";
+$depend = array(
+  "modulekit-form",        // use modulekit-form and all its requirements
+);
+
+$include = array(
+  'php' => array(
+    'inc/*.php' // automatically include all files in inc-directory
+  ),
+  'js' => array(
+    'inc/*.js' // automatically include all files in inc-directory
+  ),
+  'css' => array(
+    'style.css' // include style.css
+  )
+);
