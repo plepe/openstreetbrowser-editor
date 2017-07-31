b17a94eda6bae1565bc0d502b3c2201db345fc9b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-31 11:09:24 +0200

    Edit more values

--- index.php
+++ index.php
@@ -8,11 +8,106 @@ call_hooks("init");
 if (isset($_REQUEST['file']) && preg_match('/^[A-Za-z0-9_\-]+$/', $_REQUEST['file'])) {
   $file = "{$category_path}/{$_REQUEST['file']}.json";
 
+  $style_form_def = array(
+    'type' => 'form_chooser',
+    'order' => false,
+    'def' => array(
+      'stroke' => array(
+        'type' => 'textarea',
+        'name' => 'stroke',
+        'default' => '1',
+      ),
+      'weight' => array(
+        'type' => 'textarea',
+        'name' => 'weight',
+        'default' => '3',
+      ),
+      'color' => array(
+        'type' => 'textarea',
+        'name' => 'color',
+        'default' => '#3388ff',
+      ),
+      'opacity' => array(
+        'type' => 'textarea',
+        'name' => 'opacity',
+        'default' => '1'
+      ),
+      'lineCap' => array(
+        'type' => 'textarea',
+        'name' => 'lineCap',
+        'default' => 'round',
+      ),
+      'lineJoin' => array(
+        'type' => 'textarea',
+        'name' => 'lineJoin',
+        'default' => 'round',
+      ),
+      'dashArray' => array(
+        'type' => 'textarea',
+        'name' => 'dashArray',
+      ),
+      'dashOffset' => array(
+        'type' => 'textarea',
+        'name' => 'dashOffset',
+      ),
+      'fill' => array(
+        'type' => 'textarea',
+        'name' => '',
+      ),
+      'fillColor' => array(
+        'type' => 'textarea',
+        'name' => '#3388ff',
+      ),
+      'fillOpacity' => array(
+        'type' => 'textarea',
+        'name' => 'fillOpacity',
+        'default' => '0.2',
+      ),
+      'fillRule' => array(
+        'type' => 'textarea',
+        'name' => 'fillRule',
+      ),
+      'smoothFactor' => array(
+        'type' => 'textarea',
+        'name' => 'smoothFactor',
+        'default' => '1.0',
+      ),
+      'nodeFeature' => array(
+        'type' => 'textarea',
+        'name' => 'nodeFeature',
+        'default' => 'CircleMarker',
+      ),
+      'radius' => array(
+        'type' => 'textarea',
+        'name' => 'radius',
+        'default' => '10',
+      ),
+      'noClip' => array(
+        'type' => 'textarea',
+        'name' => 'noClip',
+        'default' => '1',
+      ),
+    ),
+  );
+
   $form_def = array(
     'query' => array(
-      'type' => 'textarea',
-      'name' => 'Query',
-      'desc' => 'Overpass QL query without "out" statement, e.g. "(node[amenity=bar];way[amenity=bar];)"',
+      'type' => 'hash',
+      'order' => false, // TODO: form could automatically order by key
+      'name' => 'Queries',
+      'desc' => '',
+      'key_def' => array(
+        'type' => 'integer',
+        'name' => 'minZoom',
+        'desc' => 'When several queries are defined, the queries are used from minZoom level to (exclusive) the next higher zoom level',
+      ),
+      'def' => array(
+        'type' => 'textarea',
+        'name' => 'Query',
+        'desc' => 'Overpass QL query without "out" statement, e.g. "(node[amenity=bar];way[amenity=bar];)"',
+      ),
+      'min' => 1,
+      'button:add_element' => 'Add query',
     ),
     'feature' => array(
       'type' => 'form_chooser',
@@ -20,6 +115,10 @@ if (isset($_REQUEST['file']) && preg_match('/^[A-Za-z0-9_\-]+$/', $_REQUEST['fil
       'name' => 'Feature evaluation',
       'desc' => 'All sub values will be evaluated via the TwigJS language',
       'def'  => array(
+        'pre' => array(
+          'type' => 'textarea',
+          'name' => 'pre',
+        ),
         'title' => array(
           'type' => 'textarea',
           'name' => 'title',
@@ -41,21 +140,38 @@ if (isset($_REQUEST['file']) && preg_match('/^[A-Za-z0-9_\-]+$/', $_REQUEST['fil
           'type' => 'textarea',
           'name' => 'priority',
         ),
+        'styles' => array(
+          'type' => 'textarea',
+          'name' => 'styles',
+        ),
       ),
+      'button:add_element' => 'Add feature code',
+    ),
+    'const' => array(
+      'type' => 'json',
+      'name' => 'const',
+      'desc' => 'Constants which can be accessed via <code>{{ const }}<code> in twig templates',
     ),
   );
 
-  $form = new form('data', $form_def);
+  foreach (array('style', 'style:casing', 'style:highlight') as $k) {
+    $form_def['feature']['def'][$k] = $style_form_def;
+    $form_def['feature']['def'][$k]['name'] = $k;
+  }
+
+  $form = new form('data', $form_def, array('type' => 'form_chooser', 'order' => false));
 
   if($form->is_complete()) {
     // save data to file
     $data = $form->save_data();
+    $data = array_merge(array('type' => 'overpass'), $data);
     file_put_contents($file, json_readable_encode($data) . "\n");
   }
 
   if ($form->is_empty()) {
     // load data from file
     $data = json_decode(file_get_contents($file), true);
+    unset($data['type']);
     $form->set_data($data);
   }
 }
