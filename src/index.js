4e245d65e4f142282c2080dae031c22da6191e9f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-12-21 22:19:44 +0100

    JS-Editor: load category from data & show map

--- src/index.js
+++ src/index.js
@@ -1,54 +1,12 @@
 var OverpassFrontend = require('overpass-frontend')
 var OpenStreetBrowser = require('openstreetbrowser')
+var jsonMultilineStrings = require('json-multiline-strings')
 
 global.options = {}
-global.map = null
 global.overpassFrontend = new OverpassFrontend('//overpass-api.de/api/interpreter')
-/*
+global.ui_lang = 'en'
+global.config = {}
 global.currentPath = null
-var currentLayer
-var currentList
-
-window.onload = function () {
-  var initState = {}
-  map = L.map('map')
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-  map.setView({ lat: config.defaultView.lat, lng: config.defaultView.lon }, config.defaultView.zoom)
-  call_hooks('init')
-  call_hooks_callback('init_callback', initState, onload2.bind(this, initState))
-
-  var previewButton = document.getElementById('preview')
-  if (previewButton) {
-    previewButton.onclick = updateMap
-  }
-}
-
-function onload2 (initState) {
-  initCategory(data, function (err) { console.log('done', err) })
-}
-
-function updateMap () {
-  currentLayer.close()
-  var data = form_data.get_data()
-  initCategory(data, function (err) { console.log('done', err) })
-}
-
-function initCategory (data, callback) {
-  currentLayer = new OpenStreetBrowser.CategoryOverpass(id, data)
-  currentLayer.load(function () {
-    currentLayer.setMap(map)
-    currentLayer.open()
-
-    var dom = document.getElementById('list')
-    dom.innerHTML = ''
-    currentLayer.setParentDom(dom)
-
-    callback(null)
-  })
-}
-*/
 
 function Editor (textarea) {
   this.textarea = textarea
@@ -66,10 +24,38 @@ Editor.prototype.isCategory = function () {
 }
 
 Editor.prototype.load = function () {
+  this.data = jsonMultilineStrings.join(this.data, { exclude: [ [ 'const' ] ] })
+
+  initState = {}
+  call_hooks('init')
+  call_hooks_callback('init_callback', initState, this.load2.bind(this, initState))
+}
+
+Editor.prototype.load2 = function (initState) {
   var div = document.createElement('div')
   this.textarea.parentNode.insertBefore(div, this.textarea)
-  div.innerHTML = 'foo'
-  // this.map = L.map('map')
+  this.textarea.style.display = 'none'
+
+  div.setAttribute('style', 'height: 300px;')
+
+  this.map = L.map(div)
+  global.map = this.map // TODO: remove this
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(this.map)
+
+  this.map.setView({ lat: 40, lng: 16 }, 14)
+
+  this.layer = new OpenStreetBrowser.CategoryOverpass('edit', this.data)
+  this.layer.load(function () {
+    this.layer.setMap(this.map)
+    this.layer.open()
+
+//    var dom = document.getElementById('list')
+//    dom.innerHTML = ''
+//    this.layer.setParentDom(dom)
+  }.bind(this))
 }
 
 Editor.prototype.reload = function () {
