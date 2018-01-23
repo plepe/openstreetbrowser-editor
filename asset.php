a6cbd461cc687a0174c1b4f3962b99994e467f12
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-01-23 21:35:59 +0100

    Forgot asset.php file

---
+++ asset.php
@@ -0,0 +1,35 @@
+<?php
+require "conf.php"; /* load configuration */
+require __DIR__ . '/vendor/autoload.php';
+include "modulekit/loader.php"; /* loads all php-includes */
+include "node_modules/openstreetbrowser/src/repositories.php";
+include "node_modules/openstreetbrowser/src/RepositoryBase.php";
+include "node_modules/openstreetbrowser/src/RepositoryDir.php";
+include "node_modules/openstreetbrowser/src/RepositoryGit.php";
+?>
+<?php call_hooks("init"); /* initialize submodules */ ?>
+<?php
+$allRepositories = getRepositories();
+
+$repoId = $_REQUEST['repo'];
+if (!array_key_exists($repoId, $allRepositories)) {
+  Header("HTTP/1.1 404 Repository not found");
+  exit(0);
+}
+
+$repoData = $allRepositories[$repoId];
+$repo = getRepo($repoId, $repoData);
+
+$tmpfile = tempnam('/tmp', 'osb-asset-');
+$contents = $repo->file_get_contents($_REQUEST['file']);
+
+if ($contents === false) {
+  Header("HTTP/1.1 401 Permission denied");
+  exit(0);
+}
+
+file_put_contents($tmpfile, $contents);
+$mime_type = mime_content_type($tmpfile);
+
+Header("Content-Type: {$mime_type}; charset=utf-8");
+print $contents;
