72e8a5dcbadf6119375d5b46bd16c214b5c74aa2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-01-25 21:26:58 +0100

    Link descriptions to Icons docu

--- src/CategoryOverpass.js
+++ src/CategoryOverpass.js
@@ -192,7 +192,7 @@ function formDef (data) {
     "type": "form_chooser",
     "order": false,
     "name": "Feature evaluation",
-    "desc": "This codes will be evaluated for each map feature. You can set different styles, texts, etc. All sub values will be evaluated via <a href=\"https://github.com/plepe/OpenStreetBrowser/blob/master/doc/TwigJS.md\">TwigJS markup</a>.",
+    "desc": "This codes will be evaluated for each map feature. You can set different styles, texts, etc. All sub values will be evaluated via <a href=\"https://github.com/plepe/OpenStreetBrowser/blob/master/doc/TwigJS.md\">TwigJS markup</a>. Fields where HTML code is expected may include <a href=\"https://github.com/plepe/OpenStreetBrowser/blob/master/doc/Icons.md\">Icons</a>.",
     "result_keep_order": true,
     "include_data": "not_null",
     "button:add_element": "Add feature code",
@@ -287,7 +287,7 @@ function formDef (data) {
   ret["info"] = {
     "type": "textarea",
     "name": "Info (e.g. map key)",
-    "desc": "You can use <a href=\"https://github.com/plepe/OpenStreetBrowser/blob/master/doc/TwigJS.md\">TwigJS markup</a> in this field.",
+    "desc": "You can use <a href=\"https://github.com/plepe/OpenStreetBrowser/blob/master/doc/TwigJS.md\">TwigJS markup</a> in this field. You may use <a href=\"https://github.com/plepe/OpenStreetBrowser/blob/master/doc/Icons.md\">Icons</a>.",
     "include_data": "not_null"
   }
 
