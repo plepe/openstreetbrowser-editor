e5541d851484e09ed43efffd07cc47c624e497fc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-08-09 08:54:15 +0200

    CategoryOverpass: empty_value

--- src/CategoryOverpass.js
+++ src/CategoryOverpass.js
@@ -59,158 +59,185 @@ class CategoryOverpass extends CategoryBase {
           "type": "textarea",
           "name": "stroke",
           "desc": "(boolean) Whether to draw stroke along the path. Set it to empty string or values '0', 'false' to disable borders on polygons or circles.",
+          "empty_value": "",
           "default": "1"
         },
         "width": {
           "type": "textarea",
           "name": "width",
           "desc": "(number) Stroke width in pixels",
+          "empty_value": "",
           "default": "3"
         },
         "color": {
           "type": "textarea",
           "name": "color",
           "desc": "(color) Stroke color, e.g. '#3388ff' or 'red'",
+          "empty_value": "",
           "default": "#3388ff"
         },
         "offset": {
           "type": "textarea",
           "name": "offset",
           "desc": "(number) Offset polylines to left (negative value) or right (positive value) in pixels.",
+          "empty_value": "",
           "default": "0"
         },
         "opacity": {
           "type": "textarea",
           "name": "opacity",
           "desc": "(number) Stroke opacity: 0..transparent, 1..fully opaque.",
+          "empty_value": "",
           "default": "1"
         },
         "lineCap": {
           "type": "textarea",
           "name": "lineCap",
           "desc": "(string) A string that defines shape to be used at the end of the stroke: 'butt', 'round' or 'square'.",
+          "empty_value": "",
           "default": "round"
         },
         "lineJoin": {
           "type": "textarea",
           "name": "lineJoin",
           "desc": "(string) A string that defines shape to be used at the corners of the stroke: 'miter', 'round', 'bevel'.",
+          "empty_value": "",
           "default": "round"
         },
         "dashArray": {
           "type": "textarea",
           "name": "dashArray",
           "desc": "(string) A string that defines the stroke dash pattern. It's a list of comma and/or white space separated numbers that specify the lengths of alternating dashes and gaps, e.g. '2,2' or '10,5,5,10'.",
+          "empty_value": ""
         },
         "dashOffset": {
           "type": "textarea",
           "name": "dashOffset",
-          "desc": "(number) Defines the distance into the dash pattern to start the dash."
+          "desc": "(number) Defines the distance into the dash pattern to start the dash.",
+          "empty_value": ""
         },
         "fill": {
           "type": "textarea",
           "name": "fill",
           "desc": "(boolean) Whether to fill the path with color. Set it to empty string or values '0', 'false' to disable filling on polygons or circles.",
+          "empty_value": ""
         },
         "fillColor": {
           "type": "textarea",
           "name": "fillColor",
           "desc": "(color) Fill color. Defaults to the value of the color option.",
-          "default": "#3388ff"
+          "default": "#3388ff",
+          "empty_value": ""
         },
         "fillOpacity": {
           "type": "textarea",
           "name": "fillOpacity",
           "desc": "Fill opacity: 0..transparent, 1..fully opaque.",
-          "default": "0.2"
+          "default": "0.2",
+          "empty_value": ""
         },
         "fillRule": {
           "type": "textarea",
           "name": "fillRule",
           "desc": "(string) A string that defines how the inside of a shape is determined: 'evenodd', 'nonzero'.",
-          "default": "evenodd"
+          "default": "evenodd",
+          "empty_value": ""
         },
         "smoothFactor": {
           "type": "textarea",
           "name": "smoothFactor",
           "desc": "(number) How much to simplify the polyline on each zoom level.",
-          "default": "1.0"
+          "default": "1.0",
+          "empty_value": ""
         },
         "nodeFeature": {
           "type": "textarea",
           "name": "nodeFeature",
           "desc": "(string) What feature type to use for nodes: 'Marker', 'Circle' or 'CircleMarker'.",
-          "default": "CircleMarker"
+          "default": "CircleMarker",
+          "empty_value": ""
         },
         "radius": {
           "type": "textarea",
           "name": "radius",
           "desc": "(number) Radius of the circle. For features of type 'Circle' in meters, for type 'CircleMarker' in pixels.",
-          "default": "10"
+          "default": "10",
+          "empty_value": ""
         },
         "noClip": {
           "type": "textarea",
           "name": "noClip",
           "desc": "(boolean) Disable polyline clipping.",
-          "default": "0"
+          "default": "0",
+          "empty_value": ""
         },
         "text": {
           "type": "textarea",
           "name": "text",
           "desc": "(string) Text along lines. See <a href=\"https://github.com/makinacorpus/Leaflet.TextPath\">documentation</a> for details",
-          "default": ""
+          "default": "",
+          "empty_value": ""
         },
         "textRepeat": {
           "type": "textarea",
           "name": "textRepeat",
           "desc": "(boolean) Specifies if the text should be repeated along the polyline.",
-          "default": "1"
+          "default": "1",
+          "empty_value": ""
         },
         "textOffset": {
           "type": "textarea",
           "name": "textOffset",
           "desc": "(number) Set an offset to position text relative to the polyline.",
-          "default": "0"
+          "default": "0",
+          "empty_value": ""
         },
         "textBelow": {
           "type": "textarea",
           "name": "textBelow",
           "desc": "(boolean) Show text below the path.",
-          "default": ""
+          "default": "",
+          "empty_value": ""
         },
         "textOrientation": {
           "type": "textarea",
           "name": "textOrientation",
           "desc": "(number/string) Rotate text. Either a value in degrees or \"flip\" or \"perpendicular\"",
           "default": "0",
+          "empty_value": ""
         },
         "textLetterSpacing": {
           "type": "textarea",
           "name": "textLetterSpacing",
           "desc": "(number/number with unit) controls spacing between text characters",
-          "default": ""
+          "default": "",
+          "empty_value": ""
         },
         "textFontSize": {
           "type": "textarea",
           "name": "textFontSize",
-          "default": "12"
+          "default": "12",
+          "empty_value": ""
         },
         "textFontFamily": {
           "type": "textarea",
           "name": "textFontFamily",
-          "default": "Arial"
+          "default": "Arial",
+          "empty_value": ""
         },
         "textFill": {
           "type": "textarea",
           "name": "textFill",
           "desc": "(color) Text color.",
-          "default": "#000000"
+          "default": "#000000",
+          "empty_value": ""
         },
         "textFillOpacity": {
           "type": "textarea",
           "name": "textFillOpacity",
           "desc": "(number) Font opacity: 0..transparent, 1..fully opaque.",
-          "default": "1"
+          "default": "1",
+          "empty_value": ""
         }
       },
     }
@@ -227,7 +254,8 @@ class CategoryOverpass extends CategoryBase {
         "pre": {
           "type": "textarea",
           "name": "pre",
-          "desc": "This code will be executed before any other code. You might want to set variables, e.g.: <code>{% set foo = tags.amenity %}</code>"
+          "desc": "This code will be executed before any other code. You might want to set variables, e.g.: <code>{% set foo = tags.amenity %}</code>",
+          "empty_value": ""
         },
         "title": {
           "type": "textarea",
@@ -246,40 +274,47 @@ class CategoryOverpass extends CategoryBase {
         "popupDescription": {
           "type": "textarea",
           "name": "popupDescription",
-          "desc": "(string) Description which will be shown in the popup."
+          "desc": "(string) Description which will be shown in the popup.",
+          "empty_value": ""
         },
         "body": {
           "type": "textarea",
           "name": "body",
-          "desc": "(string) Longer text which will be shown in the popup and in the detailed view."
+          "desc": "(string) Longer text which will be shown in the popup and in the detailed view.",
+          "empty_value": ""
         },
         "markerSign": {
           "type": "textarea",
           "name": "markerSign",
           "desc": "(string) HTML Text which will be shown in the marker (if there is a marker). You may use <a href='https://github.com/plepe/OpenStreetBrowser/blob/master/doc/Icons.md'>included icons</a>.",
+          "empty_value": ""
         },
         "listMarkerSign": {
           "type": "textarea",
           "name": "listMarkerSign",
           "desc": "(string) HTML Text which will be shown in the marker in the list. You may use <a href='https://github.com/plepe/OpenStreetBrowser/blob/master/doc/Icons.md'>included icons</a>.",
+          "empty_value": ""
         },
         "markerSymbol": {
           "type": "textarea",
           "name": "markerSymbol",
           "desc": "(string) An DIV or SVG which will be shown as marker on the centroid of the map feature. The element should have <tt>width</tt>, <tt>height</tt> attributes. Specify <tt>anchorX</tt> and <tt>anchorY</tt> attributes for positioning, otherwise it will be centered. Specify <tt>signAnchorX</tt> and <tt>signAnchorY</tt> to specify where the sign should be located (centered around this point), default: at anchor. Specify <tt>popupAnchorX</tt> and <tt>popupAnchorY</tt> to specify where the popup should open, default: at anchor.<br/>You can use the markerXYZ functions to create an SVG, e.g. <tt>{{ markerPointer({})|raw }}</tt>.",
-          "default": "{{ markerPointer({})|raw }}"
+          "default": "{{ markerPointer({})|raw }}",
+          "empty_value": ""
         },
         "listMarkerSymbol": {
           "type": "textarea",
           "name": "listMarkerSymbol",
           "desc": "(string) A symbol which will be shown in the list. Either a SVG similar to 'markerSymbol' or the strings 'line' or 'polygon' (show a line or a rectangle with the definition from the styles).",
-          "default": "{{ markerCircle({})|raw }}"
+          "default": "{{ markerCircle({})|raw }}",
+          "empty_value": ""
         },
         "listExclude": {
           "type": "textarea",
           "name": "listExclude",
           "desc": "(boolean) If true, object will not be shown in the list.",
-          "default": "false"
+          "default": "false",
+          "empty_value": ""
         },
         "listTitle": {
           "type": "textarea",
@@ -298,19 +333,22 @@ class CategoryOverpass extends CategoryBase {
         "priority": {
           "type": "textarea",
           "name": "priority",
-          "desc": "(number) Order of map features in the list. Map features with lower value will be shown first."
+          "desc": "(number) Order of map features in the list. Map features with lower value will be shown first.",
+          "empty_value": ""
         },
         "styles": {
           "type": "textarea",
           "name": "styles",
           "desc": "(string) Comma-separated list of style-ids which should be shown for this particular map features. Defaults to all styles (except 'hover').",
-          "default": "default"
+          "default": "default",
+          "empty_value": ""
         },
         "preferredZoom": {
           "type": "textarea",
           "name": "preferredZoom",
           "desc": "At which max. zoom level will the map zoom when showing details",
-          "default": "16"
+          "default": "16",
+          "empty_value": ""
         }
       }
     }
